1f19eeeb7df5da95f932a4cc770d30e1c401a7c0
marilu <romeromarilu13@gmail.com>
2019-02-21 21:44:16 -0600

    cambios

--- app/src/main/java/com/example/jorge/mediamanzana/activity_situacion_sentimental.java
+++ app/src/main/java/com/example/jorge/mediamanzana/activity_situacion_sentimental.java
@@ -5,19 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class SituacionManzana extends AppCompatActivity implements View.OnClickListener {
+public class activity_situacion_sentimental extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_situacion_manzana);
-
+        setContentView(R.layout.activity_situacion_sentimental);
         findViewById(R.id.btn_siguiente).setOnClickListener(this);
+
     }
 
     @Override
-    public void onClick(View v) {
-        Intent in = new Intent(this, Prueba.class);
+    public void onClick(View v)
+    {
+        Intent in = new Intent(this,Prueba.class);
         startActivity(in);
     }
 }
